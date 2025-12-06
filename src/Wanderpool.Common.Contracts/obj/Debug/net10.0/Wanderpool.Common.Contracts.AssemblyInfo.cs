5a8f790fbeac0ee152a00c22654deaf7ead4153b
mikedeik <sdi0800018@di.uoa.gr>
2025-12-06 21:21:38 +0200

    Add .gitignore file for project

--- src/Wanderpool.Common.Contracts/obj/Debug/net10.0/Wanderpool.Common.Contracts.AssemblyInfo.cs
+++ src/Wanderpool.Common.Contracts/obj/Debug/net10.0/Wanderpool.Common.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wanderpool.Common.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e221124f51bc9dc5ef676425a4f0a7ff94246d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3d45764fd0e7b603d93bbef830c574cb98efb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wanderpool.Common.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wanderpool.Common.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
